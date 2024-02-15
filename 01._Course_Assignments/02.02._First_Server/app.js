c200c6bbe7f88d854365ac9794597eb7d608f477
Enes Kocer <enes0539@stud.kea.dk>
2024-02-15 17:11:56 +0100

    assignment finished

--- 01._Course_Assignments/02.02._First_Server/app.js
+++ 01._Course_Assignments/02.02._First_Server/app.js
@@ -1,1 +1,27 @@
 const app = require("express") ();
+
+const drinks = [
+    { id: 1, name: "cola" },
+    { id: 2, name: "pepsi" },
+    { id: 3, name: "7up" },
+    { id: 4, name: "fanta" },
+    { id: 5, name: "Carlsberg Sport" }
+];
+
+app.get("/drinks", (req, res) => {
+    res.send({data: drinks.map(drinks => drinks.name)});
+});
+
+
+app.get("/drinks/:drinkId", (req, res) => {
+    const drinkId = Number(req.params.drinkId); 
+    const drink = drinks.find(d => d.id === drinkId);
+    
+    if (drink) {
+        res.send({ data: drink.name });
+    } else {
+        res.status(404).send({ error: "Drink not found" });
+    }
+});
+
+app.listen(8080)
