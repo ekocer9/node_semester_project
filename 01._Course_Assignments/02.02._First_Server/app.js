5f5ad0bb636056d1b2276a456d4e15b4ab7d45c9
Enes Kocer <enes0539@stud.kea.dk>
2024-02-22 22:33:37 +0100

    CRUD implemented

--- 01._Course_Assignments/02.02._First_Server/app.js
+++ 01._Course_Assignments/02.02._First_Server/app.js
@@ -12,13 +12,51 @@ app.get("/drinks", (req, res) => {
     res.send({data: drinks});
 });
 
-
 app.get("/drinks/:drinkId", (req, res) => {
     const drinkId = Number(req.params.drinkId); 
     const drink = drinks.find(d => d.id === drinkId);
     
     if (drink) {
         res.send({ data: drink.name });
+    } else {
+        res.status(404).send({ error: "Drink not found" });
+    }
+});
+
+app.post("/addDrinks", (req, res) => {
+    const newDrink = {
+        id: drinks.length + 1,
+        name: req.body.name
+    };
+
+    if (newDrink) {
+        drinks.push(newDrink);
+        res.send(newDrink)
+    } else {
+        res.status(404).send({ error: "Drink not Accepted" });
+    }
+});
+
+app.put("/updateDrinks/:drinkId", (req, res) => {
+    const drinkId = Number(req.params.drinkId);
+    const newName = req.body.name;
+    const drinkIndex = drinks.findIndex(d => d.id === drinkId);
+
+    if (drinkIndex >= 0) {
+        drinks[drinkIndex].name = newName; 
+        res.send({ data: drinks[drinkIndex] });
+    } else {
+        res.status(404).send({ error: "Drink not found" });
+    }
+});
+
+app.delete("/drinks/:drinkId", (req, res) => {
+    const drinkId = Number(req.params.drinkId);
+    const drinkIndex = drinks.findIndex(d => d.id === drinkId);
+
+    if (drinkIndex >= 0) {
+        const deletedDrink = drinks.splice(drinkIndex, 1);
+        res.send({ data: deletedDrink[0] });
     } else {
         res.status(404).send({ error: "Drink not found" });
     }
@@ -31,3 +69,4 @@ app.listen(8080, (error) => {
     }
     console.log("Server is running on port", 8080);
 });
+
