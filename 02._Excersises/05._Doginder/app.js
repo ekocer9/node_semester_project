512be6153b14651fa17b212d097792143b16062a
Enes Kocer <enes0539@stud.kea.dk>
2024-03-01 11:50:45 +0100

    DogInder 2

--- 02._Excersises/05._Doginder/app.js
+++ 02._Excersises/05._Doginder/app.js
@@ -6,11 +6,25 @@ app.use(express.static("public"))
 
 import path from "path"
 
+import getMatches from "./util/matches.js";
+
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("pulbic/homepage/homepage.html"))
+    res.sendFile(path.resolve("public/pages/homepage/homepage.html"))
 })
 
+app.get("/matches", (req, res) => {
+    res.sendFile(path.resolve("public/pages/matches/matches.html"))
+})
 
+app.get("/api/matches", (req, res) => {
+    const matches = getMatches()
+    res.send({data: matches})
+})
+
+// assignment: create a contact page
+app.get("/contact", (req, res) => {
+    res.sendFile(path.resolve("public/pages/contact/contact.html"))
+})
 
 
 
